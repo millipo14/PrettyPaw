7f7ef30b62a08802e772e3cd24f2fa4fe6016140
millipo <palina517@gmail.com>
2026-06-27 13:08:46 +0300

    deploy to docs for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/[name][ext]',
